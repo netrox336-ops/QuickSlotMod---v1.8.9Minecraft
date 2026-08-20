337aa5a6b3dacc910427f12eaf922258038a307c
Netrox <netrox336@gmail.com>
2026-08-21 00:19:00 +0500

    Исправить репозиторий Essential Loom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+        maven("https://repo.essential.gg/repository/maven-public")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://maven.architectury.dev/")
         maven("https://maven.fabricmc.net")
