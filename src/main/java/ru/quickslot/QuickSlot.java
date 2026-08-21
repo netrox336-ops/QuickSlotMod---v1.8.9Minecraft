c8ae47bd49ffc8b40b7afe478a11caa9a0f55d57
Netrox <netrox336@gmail.com>
2026-08-21 07:14:30 +0500

    Поднять версию QuickSlot до 0.6.0

--- src/main/java/ru/quickslot/QuickSlot.java
+++ src/main/java/ru/quickslot/QuickSlot.java
@@ -23,7 +23,7 @@ import ru.quickslot.inventory.InventoryManager;
 public final class QuickSlot {
     public static final String MOD_ID = "quickslot";
     public static final String MOD_NAME = "QuickSlot";
-    public static final String VERSION = "0.5.0";
+    public static final String VERSION = "0.6.0";
 
     @Mod.Instance(MOD_ID)
     public static QuickSlot instance;
